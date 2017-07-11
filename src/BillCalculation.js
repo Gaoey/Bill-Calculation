95531cfd8efc5cff5ce082e960b212534122e375
gaoey <gaoey@hotmail.com>
2017-07-11 16:59:40 +0700

    init parameter

--- src/BillCalculation.js
+++ src/BillCalculation.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
 import './BillCalculation.css';
+import Box from './CouponBox.js'
+
 
 class BillCalculation extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      people: 0,
+      code:"",
+      codeList:[]
+    };
+
+    this.submit = this.submit.bind(this);
+
+  }
+
+
+  handleInputChange(e){
+    const target = e.target;
+    const name = target.name;
+    if(name === "people"){
+      this.setState({
+        people: e.target.value,
+      });
+    }else if(name === "code"){
+      this.setState({
+        code: e.target.value,
+      })
+    }
+  }
+
+  submit(e){
+    // add the code in array
+    var codeArray = this.state.codeList;
+    codeArray.push(this.state.code);
+    this.setState({
+      codeList: codeArray,
+    });
+    e.preventDefault();
+  }
+
+
   render() {
     return (
       <div>
       <h1>Bill Calculation</h1>
-      <form>
-        <div className="section">
-        <input type="text" className="question" name="name" id="nme" required autoComplete="off" />
-        <label htmlFor="nme"><span>HOW MANY PEOPLE?</span></label>
-        </div>
-        <div className="section">
-        <input name="message" className="question" rows="2" id="msg" required autoComplete="off"></input>
-        <label htmlFor="msg"><span>COUPON CODE</span></label>
-        </div>
-        <input type="submit" value="Submit!" />
+      <form onSubmit={this.submit}>
+      <div className="section">
+      <input type="text" className="question" name="people" id="pp" required autoComplete="off" onClick={this.handleInputChange.bind(this)}/>
+      <label htmlFor="pp"><span>HOW MANY PEOPLE?</span></label>
+      </div>
+      <div className="section">
+      <input name="code" className="question" rows="2" id="cd" required autoComplete="off" onChange={this.handleInputChange.bind(this)}></input>
+      <label htmlFor="cd"><span>COUPON CODE</span></label>
+      </div>
+      <div className="section">
+      <input type="submit" value="ADD" />
+      </div>
+      <div className="section">
+      <p>{this.state.code}</p>
+      <p>{this.state.people}</p>
+      </div>
       </form>
       </div>
     );
